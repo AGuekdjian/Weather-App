dccd515c2974b3aa6dd195a149a6e15a5405ce58
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-03-25 18:49:05 -0300

    fix error

--- src/components/GetWeather/GetWeather.jsx
+++ src/components/GetWeather/GetWeather.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import CardWeather from '../CardWeather/CardWeather'
 
 export default function GetWeather() {
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=d07df994f39a4b0f9f9154719232503`
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=d07df994f39a4b0f9f9154719232503`
     const cityUrl = "&q="
     const [weather, setWeather] = useState({})
 
